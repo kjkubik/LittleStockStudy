622ff5a306a0dc4cf518a051d8f22c3f1ce02b80
Kim J Kubik <kkubik7@hotmail.com>
2024-04-01 00:00:10 -0500

    backup files in GetAllStocksDatas and counting number of days in LearningGetHighsandLows

--- source/SchedGetAllStocksData.py
+++ source/SchedGetAllStocksData.py
@@ -5,7 +5,7 @@ from GetAllStocksDatas import daily_run
 from config import stock_key
 
 # Schedule the task to run daily at a specific time (e.g., 9:00 AM)
-schedule.every().day.at("17:18").do(daily_run)
+schedule.every().day.at("04:00").do(daily_run)
 
 # Keep the program running to allow scheduled tasks to execute
 while True:
